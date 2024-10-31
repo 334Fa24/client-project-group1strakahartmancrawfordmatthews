e7ff27dc196f5e46f72bf087ad0c2a678b01bb45
Madelyn Hartman <s551202@nwmissouri.edu>
2024-10-31 13:42:46 -0500

    i hate this

--- scripts/script.js
+++ scripts/script.js
@@ -12,10 +12,11 @@ function menu() {
     }
 }
 
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
 
-// When the user clicks on <div>, open the popup
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+function closeForm() {
+  document.getElementById("myForm").style.display = "block";
 }
 
